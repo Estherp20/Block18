ede3cb9215bf229197148c7f2263153eb2824ce5
esti <estip2005@gmail.com>
2024-10-08 00:31:13 -0500

    modified it

---
+++ index.js
@@ -0,0 +1,40 @@
+// ***-- Unit Test Number 1 ---***
+
+
+// Multiply the 2 input numbers to return a product
+// Expect multiplication to return a number
+// Multiply the 2 input numbers to return a product
+// Expect multiplication with the numbers (5, 10) to equal 50
+// Expect multiplication with the numbers (5, "b") to throw an error
+
+
+// ***-- Unit Test Number 2 ---***
+
+
+// ***-- Unit Test Number 2 ---***
+
+// Make 2 arrays with integers as arguments
+// Use a function called "concatOdds"
+// Expect concatOdds([5, 8, 3], [9, 5, 1, 44, 4, 15, -1]) to return a result as an array []
+// It should return a single array that includes only the odd numbers from both arrays, sorted in ascending order.
+// Remove all of the redundant numbers
+// Expect concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to return [-1, 1, 3, 9, 15]
+// Expect concatOdds([1, 2, 3, 4], "a string") to throw an error
+
+
+
+// ***-- Function Test Number 1 ---***
+
+
+// When a user is a guest and tries to checkout, prompt them to checkout as a guest or create an account.
+// When a user checks out, the account accepts payment.
+// When a user is in checkout, provide an option to delete an item or change the quantity.
+// If the cart is empty, prompt an error when trying to checkout.
+// When a user makes a purchase as a gift, offer a gift receipt.
+// When a user checks out, display the total amount.
+// If a member/club user is checking out, display loyalty points before checkout.
+// If a member/club user is checking out, provide the option to use loyalty points before checkout.
+// When a user is checking out, provide the option of in-store pickup or delivery.
+
+
+
